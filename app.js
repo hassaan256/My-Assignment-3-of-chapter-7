7b5a54da0e6bdd915043e35a05718ab6c5a77fbc
MalikAliyan-Alam <aliyanusama557@gmail.com>
2025-09-01 17:26:34 +0500

    My Assignment 03

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Javascript connected!");
+
+// Que:1... Ans:1
+// var city = prompt("Enter a city name (Karachi) ")
+// if (city === "Karachi"){
+// console.log("Welcome to the city of Lights!");
+// } else {
+// console.log("Enter a Valid city name, Hint(Kara)!");
+// }
+
+
+// Que:2... Ans:2
+var gender = prompt("Enter your Gender.");
+if (gender === "Male"){
+console.log("Good Morning Sir.");
+} else if (gender ==="Female"){
+    console.log("Good Morning Ma'am.");
+}
